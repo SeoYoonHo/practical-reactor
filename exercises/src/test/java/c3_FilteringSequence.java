20adeff3d16adae045d98bf86a1655b39946aa05
seoyoonho <gogoy2643@naver.com>
2025-01-07 17:49:04 +0900

    3번 테스트케이스 문제 다 품(Filter)

--- exercises/src/test/java/c3_FilteringSequence.java
+++ exercises/src/test/java/c3_FilteringSequence.java
@@ -6,13 +6,13 @@ import reactor.test.StepVerifier;
 /**
  * Sequence may produce many elements, but we are not always interested in all of them. In this chapter we will learn
  * how to filter elements from a sequence.
- *
+ * <p>
  * Read first:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#which.filtering
- *
+ * <p>
  * Useful documentation:
- *
+ * <p>
  * https://projectreactor.io/docs/core/release/reference/#which-operator
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Mono.html
  * https://projectreactor.io/docs/core/release/api/reactor/core/publisher/Flux.html
@@ -26,9 +26,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void girls_are_made_of_sugar_and_spice() {
-        Flux<String> shortListed = popular_girl_names_service()
-                //todo: change this line only
-                ;
+        Flux<String> shortListed = popular_girl_names_service().filter(s -> s.length() <= 4);
 
         StepVerifier.create(shortListed)
                     .expectNext("Emma", "Ava", "Mia", "Luna", "Ella")
@@ -37,14 +35,11 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
 
     /**
      * `mashed_data_service()` returns sequence of generic objects.
-     *  Without using `filter()` operator, collect only objects that are instance of `String`
+     * Without using `filter()` operator, collect only objects that are instance of `String`
      */
     @Test
     public void needle_in_a_haystack() {
-        Flux<String> strings = null;
-        mashed_data_service()
-                //todo: change this line only
-                ;
+        Flux<String> strings = mashed_data_service().ofType(String.class);
 
         StepVerifier.create(strings)
                     .expectNext("1", "String.class")
@@ -56,9 +51,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void economical() {
-        Flux<String> items = duplicated_records_service()
-                //todo: change this line only, use only one operator
-                ;
+        Flux<String> items = duplicated_records_service().distinct();
 
         StepVerifier.create(items)
                     .expectNext("1", "2", "3", "4", "5")
@@ -68,14 +61,12 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
     /**
      * This service returns many elements, but you are only interested in the first one.
      * Also, service is very fragile, if you pull more than needed, you may brake it.
-     *
+     * <p>
      * This time no blocking. Use only one operator.
      */
     @Test
     public void watch_out_for_the_spiders() {
-        //todo: change code as you need
-        Mono<String> firstResult = Mono.empty();
-        fragile_service();
+        Mono<String> firstResult = fragile_service().next();
 
         //don't change code below
         StepVerifier.create(firstResult)
@@ -88,9 +79,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void dont_take_more_then_you_need() {
-        Flux<Integer> numbers = number_service()
-                //todo: change this line only
-                ;
+        Flux<Integer> numbers = number_service().take(100);
 
         StepVerifier.create(numbers)
                     .expectNextCount(100)
@@ -102,9 +91,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void not_a_binary_search() {
-        Flux<Integer> numbers = number_service()
-                //todo: change this line only
-                ;
+        Flux<Integer> numbers = number_service().filter(integer -> integer >= 200).take(100);
 
         StepVerifier.create(numbers)
                     .expectNextMatches(i -> i >= 200)
@@ -117,9 +104,7 @@ public class c3_FilteringSequence extends FilteringSequenceBase {
      */
     @Test
     public void golden_middle() {
-        Flux<Integer> numbers = number_service()
-                //todo: do your changes here
-                ;
+        Flux<Integer> numbers = number_service().filter(integer -> integer>=100).take(100);
 
         StepVerifier.create(numbers)
                     .expectNextMatches(i -> i >= 100)
